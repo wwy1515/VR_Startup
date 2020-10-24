e7747a8ca7b45ccefcf9e1604dd22478f16aa750
gordonkou <70184405+gordonkou@users.noreply.github.com>
2020-10-24 15:38:04 -0600

    Changed bounce settings. Added Sound to bounces.

---
+++ BasketballArcade/Assets/Scripts/BallBounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    private AudioSource bounceSound;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounceSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > 1)
+        {
+            bounceSound.Play();
+        }
+        
+    }
+}
