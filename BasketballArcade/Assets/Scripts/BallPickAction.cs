eacffc689c2ecfe3bbf2d69c972981899b8bf9d5
Song Zhang <song.zhang@utah.edu>
2020-10-24 18:44:32 -0600

    pick and throw action for Index
    Add SteamVR plugin and action.json for Index Input bindings.
    pick script for controller, and other script for ball prefab to detect pick action.

---
+++ BasketballArcade/Assets/Scripts/BallPickAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BallPickAction : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean pickball = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("PickBall");
+    SteamVR_Behaviour_Pose trackedObj;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (pickball.GetStateDown(handType))
+        {
+            // apply the pick ball action
+            // transfer the ball position to trackedObj position
+            // pass the ball GameObject to TestThrow script
+            PickThrowBasic pickThrowBasicScript = GameObject.Find("Controller (right)").GetComponent<PickThrowBasic>();
+            //Debug.Log(GameObject.Find("Controller (right)"));
+            //Debug.Log("gameObject name: " + gameObject.name);
+            //Debug.Log("testThrowScript: " + pickThrowBasicScript);
+            pickThrowBasicScript.setPickedObj(gameObject);
+
+        }
+    }
+}
---
+++ BasketballArcade/Assets/Scripts/PickThrowBasic.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickThrowBasic : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject pickedObj;
+    public Rigidbody attachPoint;
+
+    public SteamVR_Action_Boolean throwBall = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("ThrowBall");
+    public SteamVR_Action_Boolean pickball = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("PickBall");
+
+    SteamVR_Behaviour_Pose trackedObj;
+    FixedJoint joint;
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (pickedObj != null && joint == null && pickball.GetStateDown(trackedObj.inputSource))
+        {
+            GameObject go = pickedObj;
+            go.transform.position = attachPoint.transform.position;
+
+            joint = go.AddComponent<FixedJoint>();
+            joint.connectedBody = attachPoint;
+        }
+        if (joint == null && throwBall.GetStateDown(trackedObj.inputSource))
+        {
+            GameObject go = GameObject.Instantiate(prefab);
+            go.transform.position = attachPoint.transform.position;
+
+            joint = go.AddComponent<FixedJoint>();
+            joint.connectedBody = attachPoint;
+        }
+        else if (joint != null && throwBall.GetStateUp(trackedObj.inputSource))
+        {
+            GameObject go = joint.gameObject;
+            Rigidbody rigidbody = go.GetComponent<Rigidbody>();
+            Object.DestroyImmediate(joint);
+            joint = null;
+            Object.Destroy(go, 10.0f);
+
+            Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+            if (origin != null) // origin = trackedObj.origin
+            {
+                rigidbody.velocity = origin.TransformVector(trackedObj.GetVelocity());
+                rigidbody.angularVelocity = origin.TransformVector(trackedObj.GetAngularVelocity());
+                Debug.Log("velocity: " + rigidbody.velocity + "angular velocity: " + rigidbody.angularVelocity);
+            }
+            else // origin = trackedObj.transform.parent -> world space 
+            {
+                rigidbody.velocity = trackedObj.GetVelocity();
+                rigidbody.angularVelocity = trackedObj.GetAngularVelocity();
+                Debug.Log("velocity: " + rigidbody.velocity + "angular velocity: " + rigidbody.angularVelocity);
+
+            }
+
+            rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+        }
+    }
+
+    // The ball be picked should call this setter
+    public void setPickedObj(GameObject obj)
+    {
+        pickedObj = obj;
+    }
+}
