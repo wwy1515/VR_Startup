fe7ef5c315e1dd0610eb87a213a584b9b5277b05
gordonkou <70184405+gordonkou@users.noreply.github.com>
2020-12-04 15:32:32 -0700

    Shot clock resets when basket made
    Shot clock will reset to 24 sec if a basket is made.

--- BasketballArcade/Assets/Scripts/ShotClock.cs
+++ BasketballArcade/Assets/Scripts/ShotClock.cs
@@ -2,31 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ShotClock : MonoBehaviour
 {
     int clockTime = 24;
     float timeLeft = 1.0f;
     public Text clockText;
+    int current_score;
+    int compare_score;
+
+    public TMPro.TextMeshProUGUI m_ScoreText;
 
     void setClockTime()
     {
-        if (clockTime > 0)
+        if (current_score > compare_score)//if basket made
         {
-            clockTime -= 1;
-            timeLeft = 1.0f;
-        }
-        else if (clockTime == 0)
-        {
+            compare_score = current_score;
             clockTime = 24;
             timeLeft = 1.0f;
         }
+        else
+        {
+            if (clockTime > 0)
+            {
+                clockTime -= 1;
+                timeLeft = 1.0f;
+            }
+            else if (clockTime == 0)
+            {
+                clockTime = 24;
+                timeLeft = 1.0f;
+            }
+        }
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
         clockText.text = clockTime.ToString();//set shot clock to 24 sec
+        current_score = int.Parse(m_ScoreText.text);
+        compare_score = int.Parse(m_ScoreText.text);
+        
     }
 
 
@@ -34,11 +52,13 @@ public class ShotClock : MonoBehaviour
     void Update()
     {
         timeLeft -= Time.deltaTime;
+        current_score = int.Parse(m_ScoreText.text);
         if (timeLeft <= 0.0)
         {
             setClockTime();
             //Console.WriteLine(clockTime);
         }
         clockText.text = clockTime.ToString();
+
     }
 }
