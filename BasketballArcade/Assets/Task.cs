89aa3427710ba222b08e6989bbca4dc8cdfc8471
hermitli <hermitli@tencent.com>
2020-12-10 16:22:53 +0800

    WIP

--- BasketballArcade/Assets/Task.cs
+++ BasketballArcade/Assets/Task.cs
@@ -1,9 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Task : MonoBehaviour
 {
+    public float targetTime;
+    public float remainTime;
+    public int targetScore;
+    public int currentScore;
+
+    public void GenTask()
+    {
+        targetScore = (int)(Mathf.Max(1.0f, 8.0f * Random.value));
+        targetTime = (8.0f + Random.value * 4.0f) * targetScore;
+
+        currentScore = 0;
+        remainTime = targetTime;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
